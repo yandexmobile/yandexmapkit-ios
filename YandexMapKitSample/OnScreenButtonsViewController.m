6aa04119c823e635d7d6de19d0d7167cb475228d
Andrey Subbotin <andrey@subbotin.me>
2013-07-23 17:14:43 +0400

    Update copyrights

--- YandexMapKitSample/OnScreenButtonsViewController.m
+++ YandexMapKitSample/OnScreenButtonsViewController.m
@@ -3,8 +3,8 @@
  *
  * This file is a part of the Yandex Map Kit.
  *
- * Version for iOS © 2011-2012 YANDEX
- * 
+ * Version for iOS © 2011-2013 YANDEX
+ *
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://legal.yandex.ru/mapkit/
  */
